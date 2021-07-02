88d2686c62eb74135d8232fd957ca3e2c1ab0ebe
tec <yashodeepgaikwad@gmail.com>
2021-07-02 11:34:47 +0530

    commit
    demo

---
+++ demo.c
@@ -0,0 +1,15 @@
+
+
+#include<stdio.h>
+
+#include "myheader.h"
+
+main()
+{
+    int z;
+    z=add(100,200);
+    printf("\nAddition is :%d",z);
+    z=sub(200,100);
+    printf("\nSubtraction is :%d",z);
+
+}
